e044f16b0ccdd1627829f3fed36760fa3df8546b
Nicolas Ward <ultranurd@gmail.com>
2013-09-07 04:08:40 +0000

    implement a simple add_new_title filter that strips out the triangle

--- plugin.php
+++ plugin.php
@@ -1,25 +1,29 @@
 <?php
 /*
-Plugin Name: Your Plugin Name
-Plugin URI: http://github.com/your_name/your_plugin
-Description: One line description of your plugin
+Plugin Name: Remove YouTube Play Indicator
+Plugin URI: http://github.com/UltraNurd/youtube-play-indicator
+Description: Removes the triangle from the title of the shortened URL
 Version: 1.0
-Author: Your Name
-Author URI: http://your-site-if-any/
+Author: Nicolas Ward
+Author URI: http://www.ultranurd.net/
 */
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-/*
+yourls_add_filter( 'add_new_title', 'nurd_rytpi_remove_play_indicator' );
 
- Your code goes here.
- 
- Suggested read:
- https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
- https://github.com/YOURLS/YOURLS/wiki#for-developpers
- https://github.com/YOURLS/YOURLS/wiki/Plugin-List#get-your-plugin-listed-here
- 
- Have fun!
- 
-*/
+function nurd_rytpi_remove_play_indicator( $original_title, $url, $keyword = '' ) {
+	// Get the domain and check if it's from YouTube
+	$domain = parse_url( $url, PHP_URL_HOST );
+	if ( preg_match( '/(youtube|youtu\.be)/', $domain ) ) {
+		// From YouTube, so kill that triangle
+		$title = @mb_convert_encoding( preg_replace( '/^\x{25B6}\s*/u', "", $original_title ), 'UTF-8' );
+		return $title;
+	} else {
+		// Leave as-is, not from YouTube
+		return $original_title;
+	}
+}
+
+?>
